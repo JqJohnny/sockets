58fa4f2166ccfaa258120b37c63ff9fbf13e3c2e
Johnny Quach <84772584+JqJohnny@users.noreply.github.com>
2023-11-03 04:53:50 -0700

    Update socket-host.py

--- socket-host.py
+++ socket-host.py
@@ -1,6 +1,8 @@
 import socket
 import threading
-import time
+
+lock = threading.Lock()
+
 
 def handle_client(client_socket, address):
     while True:
@@ -8,7 +10,8 @@ def handle_client(client_socket, address):
             data = client_socket.recv(1024)
             if not data:
                 break
-            print(f"Received from {address}: {data.decode('utf-8')}")
+            message = data.decode('utf-8')
+            print(f"Received from {address}: {message}")
         except ConnectionResetError:
             break
     print(f"Connection with {address} closed.")
@@ -16,14 +19,19 @@ def handle_client(client_socket, address):
 
 
 def server():
+    lock.acquire()
     hostname = socket.gethostname()
     host = socket.gethostbyname(hostname)
-    port = 12345
-
+    while True:
+        port = int(input("Server port (10,000 - 20,000):"))
+        if 10000 <= port <= 20000:
+            break
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((host, port))
+
     server_socket.listen(5)
     print(f"Server listening on {host}:{port}")
+    lock.release()
 
     while True:
         client_socket, addr = server_socket.accept()
@@ -33,29 +41,29 @@ def server():
 
 
 def client():
+    lock.acquire()
     host = input("Enter IP: ")
     port = int(input("Enter port: "))
-
+    lock.release()
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((host, port))
 
     while True:
-        time.sleep(0.5)
         message = input("Enter message: ")
         if message.lower() == 'exit':
+            client_socket.send(message.encode('utf-8'))
             break
         elif message.lower() == 'disconnect':
             client_socket.close()
             break
         else:
             client_socket.send(message.encode('utf-8'))
+    client_socket.close()
 
 
 if __name__ == "__main__":
     server_thread = threading.Thread(target=server)
     server_thread.start()
-
-    time.sleep(0.5)
 
     client_thread = threading.Thread(target=client)
     client_thread.start()
